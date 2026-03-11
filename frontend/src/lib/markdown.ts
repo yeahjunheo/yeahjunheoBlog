7817d7b26c39607393ee336ac52cfb9e4ceb84bb
Yeahjun Heo <yeahjunheo@gmail.com>
2026-03-11 11:20:58 +0900

    stuff

--- frontend/src/lib/markdown.ts
+++ frontend/src/lib/markdown.ts
@@ -1,5 +1,7 @@
 import { marked } from "marked";
+import markedKatex from "marked-katex-extension";
 
+marked.use(markedKatex({ throwOnError: false }));
 marked.setOptions({ gfm: true });
 
 export function renderMarkdown(content: string): string {
