b52c77e13aa654502cd39668fb78d61b47daea81
Yeahjun Heo <yeahjunheo@gmail.com>
2026-04-02 00:25:22 +0900

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   }
